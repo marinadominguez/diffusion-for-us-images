8abfb6262a888e2e00ad1b3cc7298c80d23e96b5
Marina Domínguez Martín <marinadomin@gmail.com>
2026-01-29 10:43:51 +0100

    fix: implement ModelVarType.FIXED_LARGE in p_mean_variance for sampling/metrics

--- semantic_diffusion_model/guided_diffusion/gaussian_diffusion.py
+++ semantic_diffusion_model/guided_diffusion/gaussian_diffusion.py
@@ -635,7 +635,13 @@ class GaussianDiffusion:
                 frac = (model_var_values + 1) / 2
                 model_log_variance = frac * max_log + (1 - frac) * min_log
                 model_variance = th.exp(model_log_variance)
-                
+        elif self.model_var_type == ModelVarType.FIXED_LARGE:
+            # Like OpenAI guided-diffusion:
+            # use betas for all t>0, and posterior_variance[1] for t=0
+            model_variance = np.append(self.posterior_variance[1], self.betas[1:])
+            model_log_variance = np.log(model_variance)
+            model_variance = _extract_into_tensor(model_variance, t, x.shape)
+            model_log_variance = _extract_into_tensor(model_log_variance, t, x.shape)
         else: 
             raise NotImplementedError(self.model_var_type) 
 
